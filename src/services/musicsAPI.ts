920b687f207f7407f085744a6d58c4598a003d7c
Gabriela <gabrivicentini@icloud.com>
2024-05-29 20:46:45 -0300

    changes

--- src/services/musicsAPI.ts
+++ src/services/musicsAPI.ts
@@ -1,7 +1,7 @@
 import { AlbumType, SongType } from '../types';
 
 const getMusics = async (id: string): Promise<[AlbumType, ...SongType[]]> => {
-  const request = await fetch(`https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=${id}&entity=song`);
+  const request = await fetch(`https://corsproxy.io/?https://itunes.apple.com/lookup?id=${id}&entity=song`);
   const requestJson = await request.json();
   return requestJson.results;
 };
